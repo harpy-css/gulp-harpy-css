973669231acefde702f02e63804e583a8e59a029
jeanfredrik <jeanfredrik@gmail.com>
2016-01-21 20:48:02 +0100

    catch and forward errors

--- index.js
+++ index.js
@@ -3,30 +3,43 @@ var through = require('through2');
 var gutil = require('gulp-util');
 var vm = require('vm');
 
+var PLUGIN_NAME = 'gulp-harpy-css';
+
 module.exports = function(options) {
 	options = options || {};
-	return through.obj(function(file, encoding, callback) {
+	return through.obj(function(file, encoding, cb) {
 		if(file.isNull()) {
 			// nothing to do
-			return callback(null, file);
+			return cb(null, file);
 		}
 
 		if(file.isStream()) {
 			this.emit('error', new PluginError(PLUGIN_NAME, 'Streams not supported!'));
 		} else if(file.isBuffer()) {
 			var inputContent = file.contents.toString();
-			var vmContext = vm.createContext(Object.assign({
-				css: harpyCss.create(),
-				console: console,
-			}, options));
-			vm.runInContext(inputContent, vmContext, {
-				filename: file.path,
-			});
-			var outputContent = vmContext.css.stringify();
+			var vmContext;
+			var outputContent;
+			try {
+				console.log('hej');
+				vmContext = vm.createContext(Object.assign({
+					css: harpyCss.create(),
+					console: console,
+				}, options));
+				vm.runInContext(inputContent, vmContext, {
+					filename: file.path,
+				});
+				outputContent = vmContext.css.stringify({
+					beautify: true,
+				});
+			} catch(error) {
+				return cb(new gutil.PluginError(PLUGIN_NAME, {
+					message: error.name+' while running harpy-css\n'+error.stack,
+				}));
+			}
 
 			file.contents = new Buffer(outputContent);
 			file.path = gutil.replaceExtension(file.path, '.css');
-			return callback(null, file);
+			return cb(null, file);
 		}
 	});
 };
